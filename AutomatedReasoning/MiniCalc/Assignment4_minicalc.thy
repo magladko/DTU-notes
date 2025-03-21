1a2bee65819046546a6d4a71671feb562ac6295d
magladko <kornel.marek@gmail.com>
2025-03-22 00:48:36 +0100

    vault backup: 2025-03-22 00:48:36

--- AutomatedReasoning/MiniCalc/Assignment4_minicalc.thy
+++ AutomatedReasoning/MiniCalc/Assignment4_minicalc.thy
@@ -4,6 +4,8 @@ term \<open>((\<not> p) \<longrightarrow> p) \<longrightarrow> ((p \<longrightar
 
 term \<open>(\<forall>x. (p x)) \<longrightarrow> ((p a) \<and> (p (f a)))\<close>
 
+term \<open>(\<exists>x. (\<forall>y. (r x y))) \<longrightarrow> (\<forall>x. (\<exists>y. (r y x)))\<close>
+
 section \<open>Formula 1\<close>
 
 proposition \<open>((\<not> p) \<longrightarrow> p) \<longrightarrow> ((p \<longrightarrow> (\<not> p)) \<longrightarrow> q)\<close> by metis
@@ -188,6 +190,82 @@ qed
 
 section \<open>Formula 3\<close>
 
+proposition \<open>(\<exists>x. (\<forall>y. (r x y))) \<longrightarrow> (\<forall>x. (\<exists>y. (r y x)))\<close> by metis
+
+text \<open>
+  Predicate numbers
+    0 = r
+  Function numbers
+    0 = a
+    1 = b
+  \<close>
+
+lemma \<open>\<tturnstile>
+  [
+    Imp (Exi (Uni (Pre 0 [Var 1, Var 0]))) (Uni (Exi (Pre 0 [Var 0, Var 1])))
+  ]
+  \<close>
+proof -
+  from Imp_R have ?thesis if \<open>\<tturnstile>
+    [
+      Neg (Exi (Uni (Pre 0 [Var 1, Var 0]))),
+      Uni (Exi (Pre 0 [Var 0, Var 1]))
+    ]
+    \<close>
+    using that by simp
+  with Ext have ?thesis if \<open>\<tturnstile>
+    [
+      Uni (Exi (Pre 0 [Var 0, Var 1])),
+      Neg (Exi (Uni (Pre 0 [Var 1, Var 0])))
+    ]
+    \<close>
+    using that by simp
+  with Uni_R have ?thesis if \<open>\<tturnstile>
+    [
+      Exi (Pre 0 [Var 0, Fun 0 []]),
+      Neg (Exi (Uni (Pre 0 [Var 1, Var 0])))
+    ]
+    \<close>
+    using that by simp
+  with Ext have ?thesis if \<open>\<tturnstile>
+    [
+      Neg (Exi (Uni (Pre 0 [Var 1, Var 0]))),
+      Exi (Pre 0 [Var 0, Fun 0 []])
+    ]
+    \<close>
+    using that by simp
+  with Exi_L have ?thesis if \<open>\<tturnstile>
+    [
+      Neg (Uni (Pre 0 [Fun 1 [], Var 0])),
+      Exi (Pre 0 [Var 0, Fun 0 []])
+    ]
+    \<close>
+    using that by simp
+  with Uni_L[where t=\<open>Fun 0 []\<close>] have ?thesis if \<open>\<tturnstile>
+    [
+      Neg (Pre 0 [Fun 1 [], Fun 0 []]),
+      Exi (Pre 0 [Var 0, Fun 0 []])
+    ]
+    \<close>
+    using that by simp
+  with Ext have ?thesis if \<open>\<tturnstile>
+    [
+      Exi (Pre 0 [Var 0, Fun 0 []]),
+      Neg (Pre 0 [Fun 1 [], Fun 0 []])
+    ]
+    \<close>
+    using that by simp
+  with Exi_R[where t=\<open>Fun 1 []\<close>] have ?thesis if \<open>\<tturnstile>
+    [
+      Pre 0 [Fun 1 [], Fun 0 []],
+      Neg (Pre 0 [Fun 1 [], Fun 0 []])
+    ]
+    \<close>
+    using that by simp
+  with Basic show ?thesis
+    by simp
+qed
+
 (*
 
 # "Formula 1"
@@ -275,6 +353,36 @@ Ext
 Basic
 
 # "Formula 3"
+
+(* (\<exists>x. (\<forall>y. (r x y))) \<longrightarrow> (\<forall>x. (\<exists>y. (r y x))) *)
+
+Imp (Exi (Uni r[1, 0])) (Uni (Exi r[0, 1]))
+
+Imp_R
+  Neg (Exi (Uni r[1, 0]))
+  Uni (Exi r[0, 1])
+Ext
+  Uni (Exi r[0, 1])
+  Neg (Exi (Uni r[1, 0]))
+Uni_R
+  Exi r[0, a]
+  Neg (Exi (Uni r[1, 0]))
+Ext
+  Neg (Exi (Uni r[1, 0]))
+  Exi r[0, a]
+Exi_L
+  Neg (Uni r[b, 0])
+  Exi r[0, a]
+Uni_L[a]
+  Neg r[b, a]
+  Exi r[0, a]
+Ext
+  Exi r[0, a]
+  Neg r[b, a]
+Exi_R[b]
+  r[b, a]
+  Neg r[b, a]
+Basic
 
 *)
 
