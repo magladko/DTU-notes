2c01012116c5ed25bf99f4d294458d923a80b097
magladko <kornel.marek@gmail.com>
2025-03-22 15:29:15 +0100

    vault backup: 2025-03-22 15:29:15

---
+++ AutomatedReasoning/assignments/Assignment4_progprove.thy
@@ -0,0 +1,36 @@
+theory Assignment4_progprove imports Main begin
+
+datatype alpha = a | b
+
+inductive S :: "alpha list \<Rightarrow> bool" where
+S_empty: "S []" |
+S_wrap: "S w \<Longrightarrow> S (a # w @ [b])" |
+S_con: "S w1 \<Longrightarrow> S w2 \<Longrightarrow> S (w1 @ w2)"
+
+inductive T :: "alpha list \<Rightarrow> bool" where
+T_empty: "T []" |
+T_case: "T w1 \<Longrightarrow> T w2 \<Longrightarrow> T (w1 @ [a] @ w2 @ [b])"
+
+lemma T_S: "T w \<Longrightarrow> S w"
+  apply(induction rule: T.induct)
+   apply(rule S.S_empty)
+  by (simp add: S_con S_wrap)
+
+lemma t_con: "T w1 \<Longrightarrow> T w2 \<Longrightarrow> T (w1 @ w2)"
+  apply(induction w1 arbitrary: w2 rule: T.induct)
+   apply simp
+
+  sorry
+
+lemma S_T: "S w \<Longrightarrow> T w"
+  apply(induction rule: S.induct)
+    apply(rule T.T_empty)
+  using T_case T_empty apply fastforce
+  apply(rule t_con)
+  apply blast
+  by blast
+
+lemma "S w = T w"
+  using S_T T_S by blast
+
+end
--- AutomatedReasoning/hol/Assignment4_pureHOL.thy
+++ AutomatedReasoning/hol/Assignment4_pureHOL.thy
@@ -1,13 +1,52 @@
 theory Assignment4_pureHOL imports Pure_HOL begin
 
-lemma "(\<not> p \<longrightarrow> p) \<longrightarrow> (p \<longrightarrow> \<not> p) \<longrightarrow> q"
+section "Formula 1"
+
+proposition "(\<not> p \<longrightarrow> p) \<longrightarrow> (p \<longrightarrow> \<not> p) \<longrightarrow> q"
 proof
   assume \<open>\<not> p \<longrightarrow> p\<close>
   show \<open>(p \<longrightarrow> \<not> p) \<longrightarrow> q\<close>
   proof
     assume \<open>p \<longrightarrow> \<not> p\<close>
     show \<open>q\<close>
-      sorry
+    proof -
+      have \<open>p\<close>
+      proof (rule classical)
+        assume \<open>\<not> p\<close>
+        with \<open>\<not> p \<longrightarrow> p\<close> show \<open>p\<close> ..
+      qed
+      with \<open>p \<longrightarrow> \<not> p\<close> have "\<not> p" ..
+      from this and \<open>p\<close> have "\<bottom>" ..
+      thus "q" ..
+    qed
+  qed
+qed
+
+section "Formula 2"
+
+proposition "(\<forall>x. p x) \<longrightarrow> p a \<and> p (f a)"
+proof
+  assume \<open>\<forall>x. p x\<close>
+  show \<open>p a \<and> p (f a)\<close>
+  proof
+    from \<open>\<forall>x. p x\<close> show \<open>p a\<close> ..
+    from \<open>\<forall>x. p x\<close> show \<open>p (f a)\<close> ..
+  qed
+qed
+
+section "Formula 3"
+
+proposition "(\<exists>x. \<forall>y. r x y) \<longrightarrow> (\<forall>y. \<exists>x. r x y)"
+proof
+  assume \<open>\<exists>x. \<forall>y. r x y\<close>
+  then obtain c where \<open>\<forall>y. r c y\<close> ..
+  show \<open>\<forall>y. \<exists>x. r x y\<close>
+  proof
+    fix d
+    show \<open>\<exists>x. r x d\<close>
+    proof
+      from \<open>\<forall>y. r c y\<close> show \<open>r c d\<close> ..
+    qed
   qed
 qed
 
