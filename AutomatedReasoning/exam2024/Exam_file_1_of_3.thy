f56aea365b07d871f111f0f1992f1494dac0ed8b
magladko <kornel.marek@gmail.com>
2025-05-05 15:58:34 +0200

    vault backup: 2025-05-05 15:58:34

--- AutomatedReasoning/exam2024/Exam_file_1_of_3.thy
+++ AutomatedReasoning/exam2024/Exam_file_1_of_3.thy
@@ -9,13 +9,13 @@ Use MiniCalc to prove the following formulas and include all "Copy Result to Cli
 \<close>
 
 proposition \<open>p \<longrightarrow> \<not> (p \<and> \<not> q) \<longrightarrow> q\<close>
-  oops
+  by metis
 
 proposition \<open>(\<forall>x. p x \<and> q x) \<longrightarrow> (\<exists>x. q x \<or> r x)\<close>
-  oops
+  by metis
 
 proposition \<open>(p (f a b c) \<longrightarrow> q) \<or> (q \<longrightarrow> (\<exists>x. \<forall>y. r x y))\<close>
-  oops
+  by metis
 
 \<comment> \<open>Please keep the "end" command and ensure that Isabelle/HOL does not indicate any errors at all\<close>
 
